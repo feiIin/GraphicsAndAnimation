76d3305bef20f8074941748132f5c40a767d557f
feiIin <44904601+feiIin@users.noreply.github.com>
2019-12-03 21:45:21 +0000

    Update fs_model.glsl

--- VS2019_3D_Graphics_and_Animation_Template/01_Pipeline_Interaction/fs_model.glsl
+++ VS2019_3D_Graphics_and_Animation_Template/01_Pipeline_Interaction/fs_model.glsl
@@ -6,14 +6,45 @@ in VS_OUT
 {
     vec2 tc;
     vec3 normals;
+	vec3 pos;
 } fs_in;
 
 uniform mat4 model_matrix;
 layout(binding=0) uniform sampler2D tex;
 
+
+//Lights---------------------------------------------------
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+
+//Lights---------------------------------------------------
+
 void main(void){
   //color = vec4(1.0,1.0,1.0,1.0);
   //color = vec4(fs_in.normals, 1.0);
-  color = texture(tex, fs_in.tc);
+  //color = texture(tex, fs_in.tc);
+
+  // ambient
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+ 
+    // diffuse
+    vec3 norm = normalize(fs_in.normals);
+    vec3 lightDir = normalize(lightPos - fs_in.pos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+   
+    // specular
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - fs_in.pos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;  
+       
+    vec3 result = (ambient + diffuse + specular);// * objectColor;
+    color = vec4(result, 1.0) * texture(tex, fs_in.tc);
 
 }
